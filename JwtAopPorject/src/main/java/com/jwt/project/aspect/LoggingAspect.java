d9f0f4f79e6f4e939397193f2fd6f2d9222d825e
unknown <gmkhokababu@gmail.com>
2026-03-02 13:34:11 +0800

    update AOP

--- JwtAopPorject/src/main/java/com/jwt/project/aspect/LoggingAspect.java
+++ JwtAopPorject/src/main/java/com/jwt/project/aspect/LoggingAspect.java
@@ -1,5 +1,16 @@
 package com.jwt.project.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+	
+	@Before("execution(* com.jwt.project.service.UserService.*(..))")
+	public void logBefore() {
+		System.out.println("AOP বলছেঃ একটি ম্যাথড শুরু হতে যাচ্ছ...");
+	} 
 
 }
